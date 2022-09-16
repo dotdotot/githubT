d6a40b04cd277011eaba902a7c9bf69bb4c17eca
dotdotot <dotdotot203@naver.com>
2022-09-16 17:59:52 +0900

    asdawd

--- hello.c
+++ hello.c
@@ -1,5 +1,25 @@
+// 성직
+// -1~100까지 모두 더하기
+
+// 민형
+// -100~200까지 모두 더하기 
+
 #include <stdio.h>
 int main(){
     printf("git project start");
+
+
+    
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");
+    printf("git project start");\
     return 0;
 }
+
+// 10
+// 
